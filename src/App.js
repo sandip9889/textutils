95373d86d8978629cbf97b1b08d7bf95946e759f
sandeep <sandeep24ai048@satiengg.in>
2022-07-06 19:16:31 +0530

    textutils

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
+import About from './component/About';
+import Navbar from './component/Navbar';
+import TextForm from './component/TextForm';
+import React from 'react';
+import Alert from './component/Alert';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
+  const[mode, setMode] =useState('dark');
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+        setMode('dark');
+        document.body.style.backgroundColor = 'grey';
+    }
+    else {
+        setMode('light');
+        document.body.style.backgroundColor = 'white';
+    }
+};
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   
+<Navbar title ="DarkFire TextUtils" mode = {mode} toogleMode = {toggleMode} Aboutext = "About"/>
+<Alert Alert ="You Are In DarkFire TextUtils"/>
+<div className="container my-3">
+  
+
+<TextForm heading ="Enter the text to analyze"/>
+<About/> 
+</div>
+   </>
   );
 }
 
---
+++ src/component/TextForm.js
@@ -0,0 +1,72 @@
+import React, {useState} from 'react'
+
+
+export default function TextForm(props) {
+    const handleUpClick = () =>{
+        // console.log("upperCase was clicked: "+text)
+        let newText =text.toUpperCase();
+        setText(newText)
+    }
+
+    const handleLowClick = () =>{
+        
+        let newText =text.toLowerCase();
+        setText(newText)
+    }
+    const handleClearClick = () =>{
+        
+        let newText =""
+        setText(newText)
+    }
+    const handleCopyClick = () =>{
+        
+        var newText =document.getElementById("myBox");
+        newText.select();
+        navigator.clipboard.writeText(newText.value);
+        
+    }
+    const handleSpaceClick = () =>{
+        
+        let newText =text.split(/[ ]+/);
+        setText(newText.join(" "))
+    }
+    
+
+    
+    
+    const handleOnChange = (event) =>{
+        // console.log("On Change")
+        setText(event.target.value);
+    }
+
+    const [text, setText] = useState('');
+    // text = "new text" //wrong way to do this
+    // setText("new text")  //correct way to do this
+  return (
+    <>
+    <div className="container">
+        <h1>{props.heading}</h1>
+<div className="mb-3">
+
+  <textarea className="form-control" value={text} onChange = {handleOnChange} style={{backgroundcolour: props.mode==='dark'?'Grey':'white',color:props.mode==='dark'?'white':'black'}} id="myBox" rows="8"></textarea>
+</div>
+<button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert To Upper case </button>
+<button className="btn btn-primary mx-2 " onClick={handleLowClick}>Convert To Lower case </button>
+<button className="btn btn-primary mx-2 " onClick={handleClearClick}>Clear </button>
+<button className="btn btn-primary mx-2 " onClick={handleCopyClick}>Copy </button>
+<button className="btn btn-primary mx-2 " onClick={handleSpaceClick}>Remove Extra Remove </button>
+
+
+    </div>
+    <div className="container my-3" >
+        <h2>Your Text Summary</h2>
+        <p>{text.split(" " ).length} Words and {text.length} character</p>
+        <p>{0.008 *text.split(" " ).length} Minutes </p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+   
+   
+    </div>
+    </>
+  )
+}
